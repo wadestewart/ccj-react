714337c0110f11e1de275fd4e826037b549afdb7
Wade Stewart <stew.wade@gmail.com>
2019-02-09 23:18:03 -0500

    navbar with dropdown working

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,20 +1,32 @@
 import React, { Component } from 'react'
 
+import './Header.css'
 import navLogo from '../../imgs/ccj_logo_nav.png'
+import M from 'materialize-css'
 
 class Header extends Component {
+
+    componentDidMount = () => {
+        M.AutoInit()
+    }
 
     render() {
         return (
             <div class="navbar-fixed">
+                <ul id="dropdown1" class="dropdown-content">
+                    <li><a class="nav-scroll" href="#leadership">BOARD OF DIRECTORS</a></li>
+                    <li><a class="nav-scroll" href="#trustees">BOARD OF TRUSTEES</a></li>
+                </ul>
                 <nav>
                     <div class="nav-wrapper">
-                        <a href="/" class="brand-logo">
-                            <img className="responsive-img" src={navLogo} alt="CCJ Logo" />
-                        </a>
+                            <a href="/" class="brand-logo">
+                                <img className="responsive-img" src={navLogo} alt="CCJ Logo" />
+                            </a>
                         <ul class="right hide-on-med-and-down">
-                            <li><a href="sass.html">Sass</a></li>
-                            <li><a href="badges.html">Components</a></li>
+                            <li><a id="nav-text" href="#about">ABOUT</a></li>
+                            <li><a id="nav-text" class="dropdown-trigger" href="#!" data-target="dropdown1">LEADERSHIP<i class="material-icons right">arrow_drop_down</i></a></li>
+                            <li><a id="nav-text" href="#contact">CONTACT</a></li>
+                            <li><a id="nav-text" href="https://moz.com/beginners-guide-to-seo" target='_blank'>SUPPORT US</a></li>
                         </ul>
                     </div>
                 </nav>
