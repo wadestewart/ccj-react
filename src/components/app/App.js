84e96fe69ac4f43a0896aace41fe6cc953155652
Wade Stewart <stew.wade@gmail.com>
2019-02-09 23:50:28 -0500

    added large dark council logo to landing page

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 
 import Header from '../header/Header' 
+import DarkSquareImage from '../dark-square/DarkSquareImage'
 
 class App extends Component {
   render() {
     return (
       <div>
         <Header />
+        <DarkSquareImage />
       </div>
     );
   }
---
+++ src/components/dark-square/DarkSquareImage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import darkImage from '../../imgs/ccj_landing_logo.jpg'
+
+function DarkSquareImage() {
+    return (
+        <figure>
+            <img src={darkImage} alt="CCJ Logo" />
+        </figure>
+    )
+}
+
+export default DarkSquareImage
