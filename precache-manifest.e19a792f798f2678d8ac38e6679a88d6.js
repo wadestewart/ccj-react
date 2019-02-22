91013dfac5852577e45e07b11c4dac7fd1589be8
Wade Stewart <stew.wade@gmail.com>
2019-02-21 22:26:32 -0500

    Updates

---
+++ precache-manifest.e19a792f798f2678d8ac38e6679a88d6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "59ce7e5163a7f2c04e57",
+    "url": "/ccj-react/static/css/main.795ce6b2.chunk.css"
+  },
+  {
+    "revision": "59ce7e5163a7f2c04e57",
+    "url": "/ccj-react/static/js/main.59ce7e51.chunk.js"
+  },
+  {
+    "revision": "89eaf541266c93dbbff3",
+    "url": "/ccj-react/static/css/1.bc79321f.chunk.css"
+  },
+  {
+    "revision": "89eaf541266c93dbbff3",
+    "url": "/ccj-react/static/js/1.89eaf541.chunk.js"
+  },
+  {
+    "revision": "a3fef471b6e9aa826dd1",
+    "url": "/ccj-react/static/js/runtime~main.a3fef471.js"
+  },
+  {
+    "revision": "ad9d1a5b88db1492f7c6d3689916cee0",
+    "url": "/ccj-react/static/media/ccj_logo_nav.ad9d1a5b.png"
+  },
+  {
+    "revision": "1ea48e0207c95720e10ff9e680d6c405",
+    "url": "/ccj-react/static/media/ccj_landing_logo.1ea48e02.jpg"
+  },
+  {
+    "revision": "732bca29cfce18e1f8082b5059a935da",
+    "url": "/ccj-react/static/media/placeholder.732bca29.jpg"
+  },
+  {
+    "revision": "d544cec4123f1b299f1d9282b7ff8334",
+    "url": "/ccj-react/index.html"
+  }
+];
