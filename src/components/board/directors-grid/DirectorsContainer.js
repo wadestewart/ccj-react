171b822ccc51d48062e2af2633f3edc83e8e6123
Wade Stewart <stew.wade@gmail.com>
2019-03-24 22:17:52 -0400

    removed footer from directors component, need to debug

--- src/components/board/directors-grid/DirectorsContainer.js
+++ src/components/board/directors-grid/DirectorsContainer.js
@@ -33,10 +33,8 @@ class DirectorsContainer extends Component {
             </div>
 
             {directorsGrid}
-            
-            <div className="twitter">
-                <Footer/>
-            </div>
+
+            {/* <Footer/> */}
         </div>
     )
     }
